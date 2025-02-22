160ef3fde4c2bd4ed8ffab6ee445cc1961b16617
bharath7619 <vs_bharath2000@yahoo.com>
2025-02-22 14:18:58 +0530

    [bharath]pages refactor

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardDataStats from '../../components/CardDataStats';
 import ChartThree from '../../components/Charts/ChartThree';
 import TableOne from '../../components/Tables/TableOne';
@@ -10,14 +10,32 @@ import BedroomChildOutlinedIcon from '@mui/icons-material/BedroomChildOutlined';
 import MedicationLiquidOutlinedIcon from '@mui/icons-material/MedicationLiquidOutlined';
 import BiotechOutlinedIcon from '@mui/icons-material/BiotechOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import { useNavigate } from 'react-router-dom';
+import { Modal } from 'antd';
 
 const ECommerce: React.FC = () => {
+    const navigate = useNavigate();
+    const [isModalVisible, setIsModalVisible] = useState(false);
+  
+    const showBedsPopup = () => {
+      setIsModalVisible(true);
+    };
+  
+    const handleClose = () => {
+      setIsModalVisible(false);
+    };
+
   return (
     <>
+    <Modal title="BEDS" open={isModalVisible} onCancel={handleClose} footer={null}>
+          <h1>100 Beds available</h1>
+        </Modal>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Client's" total="3.456K" rate="0.43%" levelUp>
-          <LocalHospitalOutlinedIcon color="primary" />
-        </CardDataStats>
+        <div onClick={() => navigate("/clientList")}>
+          <CardDataStats title="Client's" total="3.456K" rate="0.43%" levelUp>
+            <LocalHospitalOutlinedIcon color="primary" />
+          </CardDataStats>
+        </div>
         <CardDataStats title="Nurse" total="2.32K" rate="4.35%" levelUp>
           <BloodtypeOutlinedIcon color="primary"/>
         </CardDataStats>
@@ -29,18 +47,24 @@ const ECommerce: React.FC = () => {
         </CardDataStats>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 pt-6">
-        <CardDataStats title="Doctors" total="3.456K" rate="0.43%" levelUp>
+        <div onClick={() => navigate("/doctorsList")}>
+        <CardDataStats  title="Doctors" total="3.456K" rate="0.43%" levelUp>
           <PersonOutlineOutlinedIcon color="primary"/>
         </CardDataStats>
+        </div>
         <CardDataStats title="Laboratorist" total="4,267K" rate="4.35%" levelUp>
           <BiotechOutlinedIcon color="primary"/>
         </CardDataStats>
+        <div onClick={() => navigate("/medicineList")}>
         <CardDataStats title="Medicines" total="$2.450K" rate="2.59%" levelUp>
             <MedicationLiquidOutlinedIcon color="primary"/>
         </CardDataStats>
+        </div>
+        <div onClick={()=> showBedsPopup()}>
         <CardDataStats title="Beds" total="3456" rate="0.95%" levelDown>
           <BedroomChildOutlinedIcon color="primary"/>
         </CardDataStats>
+        </div>
       </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
       <div className="col-span-12 xl:col-span-12">
